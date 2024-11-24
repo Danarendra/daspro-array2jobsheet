c83b5cac141daa70d827c88551a71678679291ec
Narendra <narendra.daniswara16@gmail.com>
2024-11-24 14:42:55 +0700

    Add files via upload

---
+++ Numbers19Modify.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Numbers19Modify {
+    
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][1];
+        myNumbers[0] = new int[1];
+        myNumbers[1] = new int[1];
+        myNumbers[2] = new int[1];
+
+        for (int i=0; i<myNumbers.length; i++){
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i=0; i<myNumbers.length; i++){
+            System.out.println("Length of row " + (i+1) + ": " + myNumbers[i].length);
+        }
+    }
+    
+}
